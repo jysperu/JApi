39cc2e631bd7649ad8ce25a9c40e9f9a9e7db11f
YisusCore <43710620+YisusCore@users.noreply.github.com>
2021-08-06 21:15:57 -0500

    Update app.php

--- app.php
+++ app.php
@@ -3607,7 +3607,7 @@ if ( ! class_exists('JApi'))
 
 				isset($_SERVER['REQUEST_SCHEME']) or $_SERVER['REQUEST_SCHEME'] = 'http' . ($datos['https'] ? 's' : '');
 				isset($_SERVER['SERVER_PORT'])    or $_SERVER['SERVER_PORT']    = $this -> is_command() ? 8080 : 80;
-				isset($_SERVER['REQUEST_URI'])    or $_SERVER['REQUEST_URI']    = '';
+				isset($_SERVER['REQUEST_URI'])    or $_SERVER['REQUEST_URI']    = '/';
 				isset($_SERVER['HTTP_HOST'])      or $_SERVER['HTTP_HOST']      = ($this -> is_command() ? 'coman' : 'desconoci') .'.do';
 
 				$_parsed = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
@@ -3632,7 +3632,7 @@ if ( ! class_exists('JApi'))
 					array_shift($argv); // Archivo SCRIPT
 					if (count($argv) > 0)
 					{
-						$datos['path'] = array_shift($argv);
+						$datos['path'] = '/' . array_shift($argv);
 					}
 				}
 
